fb0c23fa4456f296eee8090e25bb4bd6fe604aec
ashoplay <asksj@me.com>
2024-09-17 10:26:21 +0200

    started on loging with mongodb and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const { resolveInclude } = require("ejs");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema
 
 require("dotenv").config();
 
@@ -8,11 +12,24 @@ app.use(express.static("public")); // Serve static files (CSS, JS, etc.)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/brukerguide").then(() => console.log("connected")).catch((error) => console.log("error", error));
+const userSchema = new Schema({
+  email: String,
+  password: String
+})
+
+const user = mongoose.model("User", userSchema);
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/login", (req, res) => {
   res.render("innlogging");
+});
+app.post("/login", (req, res) => {
+  console.log(req.body)
 });
 app.get("/guide", (req, res) => {
   res.render("guide");
@@ -23,6 +40,19 @@ app.get("/dashboard", (req, res) => {
 app.get("/signinn", (req, res) => {
   res.render("signinn")
 });
+app.post("/signinn", async (req, res) => {
+  console.log(req.body)
+  const { brukernavn, password, password2} = req.body;
+
+  const newUser = new User({email: brukernavn, password, password})
+
+  await newUser.save();
+
+  console.log(result);
+
+  if(result._id) {
+    res.redirect("/dashboard");
+}});
 
 
 const PORT = process.env.PORT || 3000;
